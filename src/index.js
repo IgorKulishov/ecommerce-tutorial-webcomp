2e712071fa1ec62de0f89e5343ff5064afa2ced8
Igor Kulishov <igorkulishov@Igors-MacBook-Pro.local>
2020-03-15 21:39:06 -0400

    added tooltip

--- src/index.js
+++ src/index.js
@@ -1,24 +1,37 @@
 class EcommerceTutorial extends HTMLElement {
      constructor() {
         super();
+        this.__webcomponent;
         this.__tooltipContainer;
+        this.attachShadow({mode:"open"});
+        this.shadowRoot.innerHTML = `
+            <style>
+                #webcomponent {
+                    background-color: #44C7F4;
+                    max-width: 10rem;
+                    position: fixed;
+                    bottom: 20vh;
+                    right:5vw;
+                }
+            </style>
+            <div id="webcomponent">Web Component</div>
+        `;
      }
      connectedCallback() {
-         const webComponent = document.createElement('span');
-         webComponent.textContent = 'Web Component';
-         webComponent.addEventListener('mouseenter', this._showTooltip.bind(this));
-         webComponent.addEventListener('mouseleave', this._hideTooltip.bind(this));
-         this.appendChild(webComponent);
+         this.__webcomponent = this.shadowRoot.querySelector('#webcomponent');
+         this.__webcomponent.addEventListener('mouseenter', this._showTooltip.bind(this));
+         this.__webcomponent.addEventListener('mouseleave', this._hideTooltip.bind(this));
      }
-
      _showTooltip() {
          this.__tooltipContainer = document.createElement('div');
+         this.__tooltipContainer.style.position = 'absolute';
+         this.__tooltipContainer.style.bottom = '30px';
+         this.__tooltipContainer.style.backgroundColor = '#64dea2';
          this.__tooltipContainer.textContent = 'This is tooltip of webcomponent';
-         this.appendChild(this.__tooltipContainer);
+         this.__webcomponent.appendChild(this.__tooltipContainer);
      }
-
     _hideTooltip() {
-        this.removeChild(this.__tooltipContainer);
+        this.__webcomponent.removeChild(this.__tooltipContainer);
     }
 }
 customElements.define('ecommerce-tutorial-webcomp', EcommerceTutorial);
